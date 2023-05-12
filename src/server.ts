e2aab4478e9db12071ae42380538bbdf241aa920
minhnx8 <minhnx8@fsoft.com.vn_>
2023-05-13 01:52:29 +0700

    add config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
-import fs from 'fs';
+const fs = require('fs');
 import { log } from 'console';
 (async () => {
 
